cdc9bdef5fe0eb4158f27a9480c1b6582d15029f
Andreas <andreas.vanbarel@gmail.com>
2023-05-27 13:02:12 +0200

    Cleaned up digits.jl example

--- examples/digits.jl
+++ examples/digits.jl
@@ -1,68 +1,58 @@
-# In this script we attempt to learn to classify digits using the MNIST dataset
+# In this script we attempt to classify the digits of the MNIST dataset
 
 using Neuron
 using Statistics
-
-# Assumes these packages are installed
-using MLDatasets
-using Plots
+using MLDatasets # Assumes this is installed
 
 # Specify network and cost function
 network = Network(ReLU(784,256), ReLU(256,10), Softmax())
 cost = CrossEntropy()
 
 # Load data
-function onehot(i,n)
-    v = zeros(n)
-    v[i] = 1.0
-    return v
-end
+onehot(i,n) = (v = zeros(n); v[i] = 1.0; return v)
 function get_training_data()
     d = MNIST(split=:train)[:]
     data = [Sample(vec(d.features[:,:,i]), onehot(d.targets[i]+1, 10)) for i in 1:size(d.features,3)]
-    return data
 end 
 training_data = get_training_data();
 function get_test_data()
     d = MNIST(split=:test)[:]
     data = [Sample(vec(d.features[:,:,i]), onehot(d.targets[i]+1, 10)) for i in 1:size(d.features,3)]
-    return data
 end 
 test_data = get_test_data();
 
 # Specify network training procedure
-epochs = 3
+epochs = 1
 batch_size = 64
 optimizer = Adam(network)
 
+# Define loss and accuracy for the 
+loss(network, dataset) = mean(cost(network(s.x),s.y) for s in dataset)
+accuracy(network, dataset) = mean(argmax(network(s.x)) == argmax(s.y) for s in dataset)
+L_log = [loss(network, training_data)] # initial loss 
+A_log = [accuracy(network, training_data)] # initial accuracy 
+println("Initially, the test loss is $(L_log[1]) and the test accuracy is $(A_log[1])")
+
 # train network
-perf_log = [mean(cost(network(s.x),s.y) for s in training_data)] # initial cost
 function train()
     for epoch = 1:epochs
         println("Epoch $epoch started...")
         optimize(optimizer, cost, training_data; batch_size=batch_size)      
-        L = mean(cost(optimizer.network(s.x),s.y) for s in training_data)
-        push!(perf_log, L)
-        println("After epoch $epoch, the loss is $L")
+        push!(L_log, loss(network, training_data))
+        push!(A_log, accuracy(network, training_data))
+        println("After epoch $epoch, the test loss is $(L_log[end]) and the test accuracy is $(A_log[end])")
     end 
 end
 train()
 
-# Check accuracy 
-function accuracy(network, dataset)
-    correct = sum(argmax(network(s.x)) == argmax(s.y) for s in dataset)
-    return correct/length(dataset)
-end
-accuracy(network, training_data)
-accuracy(network, test_data)
-
 # Check a single sample
-i=3 # sample index
+i=1 # sample index
 network(training_data[i].x)
 training_data[i].y
 
 # Plot performance as function of epoch
-plot(1:length(perf_log), perf_log, xscale=:log10)
+using Plots
+plot(1:length(L_log), perf_log, xscale=:log10)
 
 # saving parameters of the model
 using JLD
